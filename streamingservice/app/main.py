73a8aa57a6fd5510e2bda90e1b2e42f48453ba7d
root <root@localhost.localdomain>
2020-09-16 15:31:07 +0800

    added consumer service

--- streamingservice/app/main.py
+++ streamingservice/app/main.py
@@ -1,4 +1,5 @@
 from confluent_kafka import Consumer, KafkaException
+import os
 import sys
 import json
 
@@ -15,16 +16,16 @@ KAFKA_TOPIC_FALSE = os.environ.get('KAFKA_TOPIC_FALSE')
 KAFKA_CONSUMER_GROUP_ID = os.environ.get('KAFKA_CONSUMER_GROUP_ID')
 
 if __name__ == '__main__':
-    broker, group, topics = sys.argv[1], sys.argv[2], sys.argv[3:]
-    conf = {'bootstrap.servers': KAFKA_BROKERS, 'group.id': group,
-	    'auto.offset.reset': 'earliest'}
-    clb = lambda consumer, p: print('Assigned partition:', p)
 
-    consumer = Consumer(conf)
+    topics = [KAFKA_TOPIC_RAW]
+    consumer = Consumer({'bootstrap.servers': KAFKA_BROKERS, 'group.id': KAFKA_CONSUMER_GROUP_ID,
+			'auto.offset.reset': 'earliest'})
+    clb = lambda consumer, p: print('Assigned partition:', p)
     consumer.subscribe(topics, on_assign=clb)
+
     try:
         while True:
-            msg = c.poll(timeout=1.0)
+            msg = consumer.poll(timeout=1.0)
             if msg is None:
                 continue
             if msg.error():
@@ -34,9 +35,3 @@ if __name__ == '__main__':
                       msg.key(), msg.value())
     finally:
         consumer.close() # ... to commit final offsets
-
-
-
-
-
-
