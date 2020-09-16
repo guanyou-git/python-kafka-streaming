bffa453c45e5a7712c8d6b3cb87c0355abc9c96a
root <root@localhost.localdomain>
2020-09-16 13:58:10 +0800

    rectify kafka broker connectivity issues

---
+++ publishservice/app/main.py
@@ -0,0 +1,40 @@
+from confluent_kafka import Producer
+import sys
+import os
+import json
+from time import sleep
+
+from generate_transactions import create_random_transaction
+
+KAFKA_BROKERS = os.environ.get('KAFKA_BROKERS')
+KAFKA_TOPIC_RAW = os.environ.get('KAFKA_TOPIC_RAW')
+KAFKA_TOPIC_NORULE = os.environ.get('KAFKA_TOPIC_NORULE')
+KAFKA_TOPIC_PENDINGVALIDATION = os.environ.get('KAFKA_TOPIC_PENDINGVALIDATION')
+KAFKA_TOPIC_TRUE = os.environ.get('KAFKA_TOPIC_TRUE')
+KAFKA_TOPIC_FALSE = os.environ.get('KAFKA_TOPIC_FALSE')
+
+KAFKA_CONSUMER_GROUP_ID = os.environ.get('KAFKA_CONSUMER_GROUP_ID')
+TRANSACTIONS_PER_SECOND = float(os.environ.get('TRANSACTIONS_PER_SECOND'))
+
+SLEEP_TIME = 1 / TRANSACTIONS_PER_SECOND
+
+
+if __name__ == '__main__':
+    broker = KAFKA_BROKERS
+    topic = KAFKA_TOPIC_RAW
+    
+    producer = Producer(**{'bootstrap.servers': broker})
+    delivery_callback = lambda err, msg: print(err or msg)
+
+    while True:
+        transaction: dict = create_random_transaction()
+        message: str = json.dumps(transaction)
+        try:
+            producer.produce(topic, message.encode(), callback=delivery_callback)
+        except BufferError:
+            print("Local producer queue is full, try again")
+        producer.poll(0)
+        producer.flush()
+        sleep(SLEEP_TIME)
+
+
--- streamingservice/app/main.py
+++ streamingservice/app/main.py
@@ -5,19 +5,18 @@ import json
 # Use Kafka producer to publish data to Kafka
 # Use Kafka consumer to consume data from Kafka
 
+KAFKA_BROKERS = os.environ.get('KAFKA_BROKERS')
+KAFKA_TOPIC_RAW = os.environ.get('KAFKA_TOPIC_RAW')
+KAFKA_TOPIC_NORULE = os.environ.get('KAFKA_TOPIC_NORULE')
+KAFKA_TOPIC_PENDINGVALIDATION = os.environ.get('KAFKA_TOPIC_PENDINGVALIDATION')
+KAFKA_TOPIC_TRUE = os.environ.get('KAFKA_TOPIC_TRUE')
+KAFKA_TOPIC_FALSE = os.environ.get('KAFKA_TOPIC_FALSE')
 
-def prepare_source():
-    # Check if topic exist, print error
-    # 
-
-
-
-
-
+KAFKA_CONSUMER_GROUP_ID = os.environ.get('KAFKA_CONSUMER_GROUP_ID')
 
 if __name__ == '__main__':
     broker, group, topics = sys.argv[1], sys.argv[2], sys.argv[3:]
-    conf = {'bootstrap.servers': broker, 'group.id': group,
+    conf = {'bootstrap.servers': KAFKA_BROKERS, 'group.id': group,
 	    'auto.offset.reset': 'earliest'}
     clb = lambda consumer, p: print('Assigned partition:', p)
 
